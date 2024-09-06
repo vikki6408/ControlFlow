a82f133439e3f83a38e521373f260ad949016afa
vikki6408 <vikki.dolt@eduvaud.ch>
2024-09-06 14:52:18 +0200

    feat: le programme avec le switch case fonctionne

---
+++ src/weeklyDataSwitchCase.js
@@ -0,0 +1,29 @@
+
+let day = 3;
+
+switch(day) {
+    case 1:
+        console.log("Lundi");
+        break;
+    case 2:
+        console.log("Mardi");
+        break;
+    case 3:
+        console.log("Mercredi");
+        break;
+    case 4:
+        console.log("Jeudi");
+        break;
+    case 5:
+        console.log("Vendredi");
+        break;
+    case 6:
+        console.log("Samedi");
+        break;
+    case 7:
+        console.log("Dimanche");
+        break;
+    default:
+        console.log("Numéro de jour invalide");
+        break;
+    }
---
+++ src/weeklyDayLoop.js
@@ -0,0 +1,10 @@
+
+let day = 3
+
+for (let i = 1; i < 8; i++){
+    if (i == day){
+        console.log(i)
+    }
+}
+
+
